f7f1b0e156a56618af2914ee9c2e1b977c360edf
Lrakulka <Olecsandr32@gmail.com>
2019-01-26 13:52:56 +0200

    Added level prefab

---
+++ Assets/Script/GenerateLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateLevel : MonoBehaviour
+{
+    public GameObject level;
+    public float generateLevelTime;
+  //  private IEnumerator 
+    // Start is called before the first frame update
+    void Start()
+    {
+       // StartCoroutine
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
