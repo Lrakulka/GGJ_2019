c11ca14bf356a7ed6c5778c05a434f2e4eeb2ba3
AskWhat <awahrhaft@gmail.com>
2019-01-26 21:36:53 +0200

    Added basic player stuff

---
+++ Assets/PlayerRelated/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+
+    public float speed = 10;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rigidbody.velocity = new Vector2(h * speed, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Action!");
+        }
+    }
+
+}
